5faa1ad77ef0fb622f89e1171bddf21865a0a9a0
C.J. Costa <ccosta@estgl.ipv.pt>
2022-10-28 11:17:24 +0100

    Exercice implementation

---
+++ comp_oper.py
@@ -0,0 +1,7 @@
+# Usando um dos operadores de comparação em Python, 
+# um programa simples de duas linhas que toma o parâmetro n como input, 
+# que é um inteiro, e imprime False Se n for menor que 100 e 
+# True Se n for maior ou igual que 100
+
+n = int(input("Enter a number: "))
+print(n >= 100)
